9e6d4557031ce64e1b80c0cb3a1adbf4e104540f
Willyan Ferdinando <willyantferdinando@gmail.com>
2023-03-04 17:23:12 -0300

    payroll-api created

---
+++ payroll-api/src/test/java/com/hrystem/payrollapi/payrollapi/PayrollApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrystem.payrollapi.payrollapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
